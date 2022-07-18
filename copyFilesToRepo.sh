b35b7130fb71aa01075b968a3fef3fb7057f6243
Harrison Thompson Buckley <harrisonthompsonbuckley@protonmail.com>
2022-07-18 21:08:02 +0800

    Added the icons with corresponding hyperlinks. Slightly re-wrote the description section of website.

---
+++ copyFilesToRepo.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+# Author: Harrison Thompson Buckley
+# Description: A small Bash script to copy the final Hugo save to the repository for website on Work Station. 
+
+cp -r /home/harrison/Programming/htbuckley.engineer/public/about /home/harrison/Programming/Repositories/htbuckley.engineer
+cp -r /home/harrison/Programming/htbuckley.engineer/public/categories /home/harrison/Programming/Repositories/htbuckley.engineer
+cp -r /home/harrison/Programming/htbuckley.engineer/public/fonts /home/harrison/Programming/Repositories/htbuckley.engineer
+cp -r /home/harrison/Programming/htbuckley.engineer/public/posts /home/harrison/Programming/Repositories/htbuckley.engineer
+cp -r /home/harrison/Programming/htbuckley.engineer/public/series /home/harrison/Programming/Repositories/htbuckley.engineer
+cp -r /home/harrison/Programming/htbuckley.engineer/public/tags /home/harrison/Programming/Repositories/htbuckley.engineer
+cp /home/harrison/Programming/htbuckley.engineer/public/404.html /home/harrison/Programming/Repositories/htbuckley.engineer
+cp /home/harrison/Programming/htbuckley.engineer/public/bundle.min.feebdc8d801e3fb1791305e362b6d11557f029604dd5b3c27e2686bc88787ee90ae1ccf0afe81bd7d959d7617cfe1fc73062aa82b4e1fa830d6941193d2cbddf.js /home/harrison/Programming/Repositories/htbuckley.engineer
+cp /home/harrison/Programming/htbuckley.engineer/public/index.html /home/harrison/Programming/Repositories/htbuckley.engineer
+cp /home/harrison/Programming/htbuckley.engineer/public/index.xml /home/harrison/Programming/Repositories/htbuckley.engineer
+cp /home/harrison/Programming/htbuckley.engineer/public/main.4e5c639214707eff609bb55fe49e183dee42258a73bc90e4cc7b0a84f900798a.css /home/harrison/Programming/Repositories/htbuckley.engineer
+cp /home/harrison/Programming/htbuckley.engineer/public/main.css.map /home/harrison/Programming/Repositories/htbuckley.engineer
+cp /home/harrison/Programming/htbuckley.engineer/public/robots.txt /home/harrison/Programming/Repositories/htbuckley.engineer
+cp /home/harrison/Programming/htbuckley.engineer/public/sitemap.xml /home/harrison/Programming/Repositories/htbuckley.engineer
